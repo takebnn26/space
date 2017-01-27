be1c6627f93e5b91887260d5442d540958c2b65b
hayato takemoto <takebnn26@gmail.com>
2017-01-27 19:55:48 +0900

    fix style of argument

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit :sign_up, keys: [:name]
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
-  def after_sign_in_path_for resource
+  def after_sign_in_path_for(resource)
     root_path
   end
 end
