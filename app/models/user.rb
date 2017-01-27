b25296ef8c1a103cb7215ae24562972f291318cc
hayato takemoto <takebnn26@gmail.com>
2017-01-27 12:19:17 +0900

    add validation to name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates presence: true
+
 end
