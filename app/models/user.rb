e8972868ee8bfbd747e3c864faef66fad5c499f1
hayato takemoto <takebnn26@gmail.com>
2017-02-04 20:00:47 +0900

    preparate user_model_test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
 end
