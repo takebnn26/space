27b259f8449e299cd2ad3f3a259dd6b85edfbef1
hayato takemoto <takebnn26@gmail.com>
2017-02-07 21:55:12 +0900

    add collection check boxes

--- app/controllers/groups/messages_controller.rb
+++ app/controllers/groups/messages_controller.rb
@@ -1,6 +1,6 @@
 class Groups::MessagesController < ApplicationController
   def index
-    @group = Group.find(params[:group_id])
+    @group = current_user.groups.find(params[:group_id])
     @groups = current_user.groups
   end
 end
