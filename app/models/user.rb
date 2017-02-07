9882760509ef26b94994fd9f9684b6d6442871bf
hayato takemoto <takebnn26@gmail.com>
2017-02-07 16:34:27 +0900

    fix conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-<<<<<<< HEAD
   has_many :groups, through: :group_users
   has_many :group_users, dependent: :destroy
 
-  validates :name, presence: true, uniquness: true
-=======
   validates :name, presence: true, uniqueness: true
->>>>>>> master
 
 end
