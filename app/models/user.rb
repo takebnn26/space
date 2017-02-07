844a72413217ab7fdf8db3c897c55b08ecf64fe2
hayato takemoto <takebnn26@gmail.com>
2017-02-07 16:06:33 +0900

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   has_many :groups, through: :group_users
   has_many :group_users, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, uniquness: true
 
 end
