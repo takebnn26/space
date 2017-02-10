a26872603acf3589e9ba8871a9899bc259e09a5b
hayato takemoto <takebnn26@gmail.com>
2017-02-11 02:29:04 +0900

    create js for add members and remove members

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -5,11 +5,31 @@ $( function() {
   function appendUser(user) {
     var html = `<li class='chat-group-user clearfix'>
                   <div class='chat-group-user__name'>${ user.name }</div>
-                  <div class='chat-group-user__btn chat-group-user__btn--add'>追加</div>
+                  <div class='chat-group-user__btn chat-group-user__btn--add'
+                   data-name='${ user.name }' data-id='${ user.id }'>追加</div>
                 </li>`;
 
     $('#user-search-result').append(html);
   };
+
+  $('#user-search-result').on('click', '.chat-group-user__btn--add', function(){
+    var name = $(this).data('name');
+    var id   = $(this).data('id');
+
+    var html = `<li class='chat-group-user clearfix'>
+                  <div class='chat-group-user__name'>${ name }</div>
+                  <div class='chat-group-user__btn
+                   chat-group-user__btn--remove'>削除</div>
+                  <input type='hidden' value='${ id }' name='group[user_ids][]'>
+                </li>`;
+
+    $('#chat-group-users').append(html);
+    $(this).parent().remove();
+  });
+
+  $('#chat-group-users').on('click', '.chat-group-user__btn--remove', function() {
+    $(this).parent().remove();
+  });
 
   $('#user-search-field').on('keyup', function() {
 
